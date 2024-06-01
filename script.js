17fcfec1796ecc187ff912374ded7331710e107a
Dfong1 <103082807+Dfong1@users.noreply.github.com>
2024-06-01 17:00:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,16 @@ const rightItems = document.querySelector('.nav-mobile .right-items');
 const containerAll = document.querySelector('.container__all');
 
 document.addEventListener('DOMContentLoaded', function() {
+    facebook_icon.addEventListener('click', function() {
+        window.open('https://www.facebook.com/profile.php?id=61559499794684', '_blank')
+    })
+
+    instagram_icon.addEventListener('click', function() {
+        window.open('https://www.instagram.com/tecnodev12/', '_blank')
+    })
+    email_icon.addEventListener('click', function() {
+        window.open('mailto:tecnodev12@gmail.com', '_self')
+    })
     const burgerMenu = document.getElementById('burger-menu');
     const rightItems = document.querySelector('.nav-mobile .right-items');
     const overlay = document.createElement('div');
