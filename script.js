a7a33d771fe2dfea3c82960f39d04538297e118b
yixxuz18 <yisuskroom@gmail.com>
2024-06-04 08:56:05 -0600

    dark mode

--- script.js
+++ script.js
@@ -66,3 +66,26 @@ function lazyLoad() {
 	
 }
 });
+
+
+document.addEventListener('DOMContentLoaded', (event) => {
+    const toggleSwitch = document.getElementById('dark-mode-toggle');
+    const currentTheme = localStorage.getItem('theme');
+
+    if (currentTheme) {
+        document.body.classList.add(currentTheme);
+        if (currentTheme === 'dark-mode') {
+            toggleSwitch.checked = true;
+        }
+    }
+
+    toggleSwitch.addEventListener('change', function() {
+        if (this.checked) {
+            document.body.classList.add('dark-mode');
+            localStorage.setItem('theme', 'dark-mode');
+        } else {
+            document.body.classList.remove('dark-mode');
+            localStorage.setItem('theme', 'light-mode');
+        }
+    });
+});
